ed7c1c2f82c773a7db14d1ca5f83a04c8a5eb3a8
Sean Pipkin <seanp@assafmedia.co.il>
2025-03-28 16:13:40 +0300

    added mainScreen

--- my-app/src/components/screen/MainScreen.js
+++ my-app/src/components/screen/MainScreen.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { Container, Button } from "./MainScreen.styles";
+import { Container, ButtonContainer, Button, ShapeContainer, Rectangle, Circle, FlexDiv } from "./MainScreen.styles";
 
-
-
+// @TODO split the main screen into 2 components, a visual and a text 
 const MainScreen = ({ onOpenPopup }) => {
   return (
     <Container>
-      <Button onClick={onOpenPopup}>Open Popup</Button>
+      <ButtonContainer>
+        <Button>Text</Button>
+        <Button>Visual</Button>
+        <Button onClick={onOpenPopup}>+</Button>
+      </ButtonContainer>
+
+      <ShapeContainer>
+        <Rectangle>
+          <FlexDiv>
+            <div>3000</div>
+            <div>2000</div>
+            <div>1000</div>
+            <div>0</div>
+          </FlexDiv>
+        </Rectangle>
+        <Circle />
+        <Circle />
+      </ShapeContainer>
     </Container>
   );
 };
--- my-app/src/components/screen/MainScreen.styles.js
+++ my-app/src/components/screen/MainScreen.styles.js
@@ -1,16 +1,57 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  background: #f5f5f5;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: 100%;
+    height: 100vh;
+    background: #f5f5f5;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    justify-content: space-evenly;
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 20px;
 `;
 
 export const Button = styled.button`
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    margin: 0 10px;
+`;
+
+export const ShapeContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 60%;
+    gap: 5%;
+`;
+
+export const Rectangle = styled.div`
+    width: 7%;
+    height: 100%;
+    background-color: #3498db;
+    margin-right: 20px;
+`;
+
+export const Circle = styled.div`
+    width: 20%;
+    height: 60%;
+    background-color: #e74c3c;
+    border-radius: 50%;
+`;
+
+export const FlexDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    height: 100%;
 `;
